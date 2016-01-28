5e52bf7d7561fade353da3ee7369897cb3879c29
Ben Densmore <bdensmore@densmore.net>
2016-01-28 20:00:06 +0000

    Adding dita files

---
+++ out/webhelp-feedback/oxygen-webhelp/search/htmlFileList.js
@@ -0,0 +1,28 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "concepts/autumnFlowers.html";
+fl["1"]= "concepts/glossaryBulb.html";
+fl["2"]= "concepts/glossaryCultivar.html";
+fl["3"]= "concepts/glossaryGenus.html";
+fl["4"]= "concepts/glossaryPanicle.html";
+fl["5"]= "concepts/glossaryPerennial.html";
+fl["6"]= "concepts/glossaryPollination.html";
+fl["7"]= "concepts/glossaryRhizome.html";
+fl["8"]= "concepts/glossarySepal.html";
+fl["9"]= "concepts/springFlowers.html";
+fl["10"]= "concepts/summerFlowers.html";
+fl["11"]= "concepts/winterFlowers.html";
+fl["12"]= "tasks/gardenPreparation.html";
+fl["13"]= "tasks/pruning.html";
+fl["14"]= "topics/care.html";
+fl["15"]= "topics/copyright.html";
+fl["16"]= "topics/index.html";
+fl["17"]= "topics/introduction.html";
+fl["18"]= "topics/flowers/chrysanthemum.html";
+fl["19"]= "topics/flowers/gardenia.html";
+fl["20"]= "topics/flowers/gerbera.html";
+fl["21"]= "topics/flowers/iris.html";
+fl["22"]= "topics/flowers/lilac.html";
+fl["23"]= "topics/flowers/salvia.html";
+fl["24"]= "topics/flowers/snowdrop.html";
+var doStem = false;searchLoaded = true;
